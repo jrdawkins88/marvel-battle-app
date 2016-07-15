bc3b43eff054261f7a4cfcb421d1bc6bff6a5d67
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2016-07-15 12:22:46 -0400

    Fixed route parameters

--- src/js/App/AppRouter.js
+++ src/js/App/AppRouter.js
@@ -6,19 +6,12 @@ var AppRouter = Backbone.Router.extend({
 
 	routes: {
         'main': 'main',
-        'main/,': 'main',
-        'main/,:right': 'right',
-        'main/:left,': 'main',
-        'main/:left,:right': 'main'   // /main/48102,23812
+        'main/(:left),(:right)': 'main'
 	},
 
 	main: function (left, right) {
 		main.showMain(left, right);
-	},
-
-    right: function (right) {
-        main.showMain(null, right);
-    }
+	}
 
 });
 
